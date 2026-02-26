64b90a9363819e7d0e1f6172a3fe2a56be2cc4e5
Besarta Jonuzi <s2307401@student.hb.se>
2026-02-26 17:47:33 +0100

    db

--- SpaceshipGame.DataLayer/ApplicationDbContextFactory.cs
+++ SpaceshipGame.DataLayer/ApplicationDbContextFactory.cs
@@ -1,22 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Data.Sqlite; // Add this using directive
+// or, if you are using the Microsoft.EntityFrameworkCore.Sqlite package, use:
+using Microsoft.EntityFrameworkCore.Sqlite;
 
 namespace SpaceshipGame.DataLayer
 {
     public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
         public ApplicationDbContextFactory() { }
+
         public ApplicationDbContext CreateDbContext(string[] args)
         {
             //var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             //optionsBuilder.UseSqlite("Data Source=spaceshipgame.db");
             //return new ApplicationDbContext(optionsBuilder.Options);
 
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json").Build(); 
-            var connectionString = builder.GetConnectionString("DefaultConnection");
+                .AddJsonFile("appsettings.json").Build();
+            var connectionString = builder.GetConnectionString("SpaceshipGameDB");
             if (string.IsNullOrEmpty(connectionString))
                 throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
